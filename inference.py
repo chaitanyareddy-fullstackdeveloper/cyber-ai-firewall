75f46cf9813f86009e8ea6b723552393046d85ea
Name <user@gmail.com>
2026-04-11 09:26:46 +0530

    port updated inference.py

--- inference.py
+++ inference.py
@@ -99,4 +99,4 @@ def get_state():
 # =====================================================
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run(app, host="127.0.0.1", port=7860)
